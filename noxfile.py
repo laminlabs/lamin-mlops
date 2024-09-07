d7b5c4927954f6d3321dfaeac580f97f80b2ba76
Alex Wolf <f.alexander.wolf@gmail.com>
2024-09-07 16:09:32 +0200

    ♻️ Refactor

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,8 @@
 import nox
-from laminci import upload_docs_artifact
-from laminci.nox import build_docs, run_pre_commit, run_pytest, run
+from subprocess import run
+from laminci import upload_docs_artifact, run_notebooks
+from laminci.nox import build_docs, run_pre_commit
 
-# we'd like to aggregate coverage information across sessions
-# and for this the code needs to be located in the same
-# directory in every github action runner
-# this also allows to break out an installation section
 nox.options.default_venv_backend = "none"
 
 
@@ -17,9 +14,9 @@ def lint(session: nox.Session) -> None:
 @nox.session()
 def build(session):
     run(
-        session,
         "uv pip install --system 'lamindb[jupyter,aws]' torch torchvision lightning wandb",
+        shell=True,
     )
-    run_pytest(session)
+    run_notebooks("./docs")
     build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
