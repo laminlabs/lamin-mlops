f473dd9439e2b16f58dee7635de124aca3b156b3
Lukas Heumos <lukas.heumos@posteo.net>
2025-04-10 15:39:19 +0200

    🎨 Polish
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- docs/autoencoder.py
+++ docs/autoencoder.py
@@ -1,9 +1,11 @@
-from torch import optim, nn
+import torch
 import lightning
+
+from torch import optim, nn
 
 
 class LitAutoEncoder(lightning.LightningModule):
-    def __init__(self, hidden_size, bottleneck_size):
+    def __init__(self, hidden_size: int, bottleneck_size: int) -> None:
         super().__init__()
         self.encoder = nn.Sequential(
             nn.Linear(28 * 28, hidden_size),
@@ -17,7 +19,9 @@ class LitAutoEncoder(lightning.LightningModule):
         )
         self.save_hyperparameters()
 
-    def training_step(self, batch, batch_idx):
+    def training_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
         x, y = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
@@ -26,6 +30,6 @@ class LitAutoEncoder(lightning.LightningModule):
         self.log("train_loss", loss)
         return loss
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> optim.Optimizer:
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
