0b9b54ce2268aac0fbf151977d48884663b07bce
Lukas Heumos <lukas.heumos@posteo.net>
2025-04-10 14:52:13 +0200

    🎨 Group
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,11 @@
+from pathlib import Path
 import nox
-from laminci import upload_docs_artifact, run_notebooks
+from laminci import upload_docs_artifact
 from laminci.nox import run_pre_commit, build_docs, run
+
+
+GROUPS = {}
+GROUPS["mlops"] = ["mnist.ipynb", "wandb.ipynb", "mlflow.ipynb"]
 
 
 @nox.session
@@ -8,12 +13,21 @@ def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
+@nox.session
+@nox.parametrize(
+    "group",
+    [
+        "mlops",
+    ],
+)
 @nox.session()
-def build(session):
+def build(session, group):
     run(
         session,
         "uv pip install --system 'lamindb[jupyter]' torchvision lightning wandb mlflow",
     )
-    run_notebooks("./docs")
+    run(session, f"pytest -s ./tests/test_notebooks.py::test_{group}")
+    for path in Path(f"./docs_{group}").glob("*"):
+        path.rename(f"./docs/{path.name}")
     build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
