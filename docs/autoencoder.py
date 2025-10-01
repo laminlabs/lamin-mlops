e4af129968a248a805745132a36f8c9b517b4fbc
Lukas Heumos <lukas.heumos@posteo.net>
2025-10-01 13:28:16 +0200

    🎨 Metric annotation
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- docs/autoencoder.py
+++ docs/autoencoder.py
@@ -28,6 +28,16 @@ class LitAutoEncoder(lightning.LightningModule):
         x_hat = self.decoder(z)
         loss = nn.functional.mse_loss(x_hat, x)
         self.log("train_loss", loss, on_epoch=True)
+        return loss
+
+    def validation_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = nn.functional.mse_loss(x_hat, x)
         self.log("val_loss", loss, on_epoch=True)
         return loss
 
