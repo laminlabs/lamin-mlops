a01b4d80b8e85efb012dd5a03dcd5a24e34b977e
Alex Wolf <f.alexander.wolf@gmail.com>
2026-04-12 16:04:34 +0200

    ♻️ Back to main

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,9 @@
-from pathlib import Path
-import nox
 import os
+from pathlib import Path
+
+import nox
 from laminci import convert_executable_md_files, upload_docs_artifact
-from laminci.nox import run_pre_commit, build_docs, run, install_lamindb
+from laminci.nox import build_docs, install_lamindb, run, run_pre_commit
 
 IS_PR = os.getenv("GITHUB_EVENT_NAME") != "push"
 
@@ -24,7 +25,7 @@ def lint(session: nox.Session) -> None:
     ],
 )
 def build(session, group):
-    install_lamindb(session, branch="refactor-lightning-integration")
+    install_lamindb(session, branch="main")
     convert_executable_md_files()
     run(
         session,
